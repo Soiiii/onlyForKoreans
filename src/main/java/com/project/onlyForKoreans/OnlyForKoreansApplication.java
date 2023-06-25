71cf9a450ae2bcc66995f5ef82d215bef52caf7d
Soiiii <cojomkr33@gmail.com>
2023-06-25 16:51:04 +0900

    commit' ' wq q qgit add . e > exit !

---
+++ src/main/java/com/project/onlyForKoreans/OnlyForKoreansApplication.java
@@ -0,0 +1,14 @@
+package com.project.onlyForKoreans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyForKoreansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyForKoreansApplication.class, args);
+		//new
+	}
+
+}
